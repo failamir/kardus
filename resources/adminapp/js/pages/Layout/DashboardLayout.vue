0f5ed60a627b028554b696684060da88a9bbf894
Laravel Daily <info@laraveldaily.com>
2022-05-05 12:42:28 +0000

    QuickAdminPanel automatic commit

---
+++ resources/adminapp/js/pages/Layout/DashboardLayout.vue
@@ -0,0 +1,108 @@
+<template>
+  <div class="wrapper" :class="{ 'nav-open': $sidebar.showSidebar }">
+    <event-hub></event-hub>
+    <side-bar :sidebarLinks="sidebarLinks">
+      <mobile-menu slot="content"></mobile-menu>
+    </side-bar>
+
+    <div class="main-panel">
+      <top-navbar></top-navbar>
+      <div class="content">
+        <dashboard-content></dashboard-content>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import DashboardContent from './Content.vue'
+import TopNavbar from './TopNavbar.vue'
+import MobileMenu from './MobileMenu.vue'
+
+export default {
+  components: {
+    DashboardContent,
+    TopNavbar,
+    MobileMenu
+  },
+  data() {
+    return {
+      sidebarLinks: [
+        {
+          title: 'global.dashboard',
+          icon: 'dashboard',
+          path: { name: 'dashboard' }
+        },
+        {
+          title: 'cruds.userManagement.title',
+          icon: 'person',
+          path: { name: 'user_management' },
+          gate: 'user_management_access',
+          children: [
+            {
+              title: 'cruds.permission.title',
+              icon: 'perm_data_setting',
+              path: { name: 'permissions.index' },
+              gate: 'permission_access'
+            },
+            {
+              title: 'cruds.role.title',
+              icon: 'group',
+              path: { name: 'roles.index' },
+              gate: 'role_access'
+            },
+            {
+              title: 'cruds.user.title',
+              icon: 'person',
+              path: { name: 'users.index' },
+              gate: 'user_access'
+            }
+          ]
+        },
+        {
+          title: 'cruds.course.title',
+          icon: 'table_view',
+          path: { name: 'courses.index' },
+          gate: 'course_access'
+        },
+        {
+          title: 'cruds.lesson.title',
+          icon: 'table_view',
+          path: { name: 'lessons.index' },
+          gate: 'lesson_access'
+        },
+        {
+          title: 'cruds.test.title',
+          icon: 'table_view',
+          path: { name: 'tests.index' },
+          gate: 'test_access'
+        },
+        {
+          title: 'cruds.question.title',
+          icon: 'table_view',
+          path: { name: 'questions.index' },
+          gate: 'question_access'
+        },
+        {
+          title: 'cruds.questionOption.title',
+          icon: 'table_view',
+          path: { name: 'question_options.index' },
+          gate: 'question_option_access'
+        },
+        {
+          title: 'cruds.testResult.title',
+          icon: 'table_view',
+          path: { name: 'test_results.index' },
+          gate: 'test_result_access'
+        },
+        {
+          title: 'cruds.testAnswer.title',
+          icon: 'table_view',
+          path: { name: 'test_answers.index' },
+          gate: 'test_answer_access'
+        }
+      ]
+    }
+  }
+}
+</script>
---
+++ routes/api.php
@@ -0,0 +1,43 @@
+<?php
+
+Route::group(['prefix' => 'v1', 'as' => 'api.', 'namespace' => 'Api\V1\Admin', 'middleware' => ['auth:sanctum']], function () {
+    // Abilities
+    Route::apiResource('abilities', 'AbilitiesController', ['only' => ['index']]);
+
+    // Locales
+    Route::get('locales/languages', 'LocalesController@languages')->name('locales.languages');
+    Route::get('locales/messages', 'LocalesController@messages')->name('locales.messages');
+
+    // Permissions
+    Route::resource('permissions', 'PermissionsApiController');
+
+    // Roles
+    Route::resource('roles', 'RolesApiController');
+
+    // Users
+    Route::resource('users', 'UsersApiController');
+
+    // Courses
+    Route::post('courses/media', 'CoursesApiController@storeMedia')->name('courses.storeMedia');
+    Route::resource('courses', 'CoursesApiController');
+
+    // Lessons
+    Route::post('lessons/media', 'LessonsApiController@storeMedia')->name('lessons.storeMedia');
+    Route::resource('lessons', 'LessonsApiController');
+
+    // Tests
+    Route::resource('tests', 'TestsApiController');
+
+    // Questions
+    Route::post('questions/media', 'QuestionsApiController@storeMedia')->name('questions.storeMedia');
+    Route::resource('questions', 'QuestionsApiController');
+
+    // Question Options
+    Route::resource('question-options', 'QuestionOptionsApiController');
+
+    // Test Results
+    Route::resource('test-results', 'TestResultsApiController');
+
+    // Test Answers
+    Route::resource('test-answers', 'TestAnswersApiController');
+});
